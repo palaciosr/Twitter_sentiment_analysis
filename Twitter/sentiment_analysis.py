091a6a181a01c35630b4271036e33a6eaa6170ef
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-06-13 11:58:01 -0700

    Used textblob to get sentiment analysis

--- Twitter/sentiment_analysis.py
+++ Twitter/sentiment_analysis.py
@@ -1,4 +1,22 @@
 from textblob import TextBlob
 
+from get_tweets import getTweets
+
 
 #will get the sentiment for the tweets that capture 
+
+
+
+def SentimentOnTweets():
+   tweets_in_search=getTweets()
+   sentiments=[]
+
+   for i in range(len(tweets_in_search)):
+
+      blob=TextBlob(str(tweets_in_search[i]))
+      sentiments.append(blob.sentiment)
+   
+   return sentiments
+
+sentiment_tweet=SentimentOnTweets()
+print(sentiment_tweet)
